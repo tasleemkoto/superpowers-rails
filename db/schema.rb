be644b0168af28c4fdc58387c6c793a8e47be373
tasleem kotowaroo <tasleem.kotowaroo@nantes.archi>
2024-11-02 13:42:30 +0400

    remove type from superpowers, show html

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_31_122416) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_01_152557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,7 +29,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_31_122416) do
     t.string "title"
     t.text "description"
     t.integer "price"
-    t.string "type"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
