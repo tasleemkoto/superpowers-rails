7ca50fc04f354a41a994fcbc9fc2dbf3f7f8a9a0
vidoor mathoora <shyamalmathoora@gmail.com>
2024-11-07 19:25:04 +0400

    added credit method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:update, :destroy]
   def index
     @bookings = current_user.bookings
     @superpowers = current_user.superpowers
@@ -10,7 +11,7 @@ class BookingsController < ApplicationController
   end
 
   def show
-    @bookings
+    @booking
   end
 
   def create
@@ -18,10 +19,20 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.superpower = @superpower
-    if @booking.save
-      redirect_to bookings_path
+
+    rental_days = (@booking.end_date - @booking.start_date).to_i
+    total_price = rental_days * @superpower.renting_price
+
+    if current_user.credits >= total_price
+      # @booking.total_price = total_price 
+      if @booking.save
+        redirect_to bookings_path, notice: "Booking created successfully. Total amount: $#{total_price}"
+      else
+        render :new, status: :unprocessable_entity
+      end
     else
-      render :new, status: :unprocessable_entity
+      flash[:alert] = "Not enough credits to book this superpower"
+      redirect_to superpower_path(@superpower)
     end
   end
 
@@ -30,11 +41,13 @@ class BookingsController < ApplicationController
   end
 
   def update
-    set_booking
-    if @booking.update(booking_params)
-      redirect_to bookings_path
+    if booking_params[:status] == "accepted" && @booking.update(booking_params)
+      transfer_credits
+      redirect_to bookings_path, notice: "Booking accepted, and credits transferred successfully."
+    elsif @booking.update(booking_params)
+      redirect_to bookings_path, notice: "Booking updated successfully."
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -44,6 +57,23 @@ class BookingsController < ApplicationController
   end
 
   private
+
+  def transfer_credits
+    if @booking.status == "accepted"
+      rental_days = (@booking.end_date - @booking.start_date).to_i
+      total_price = rental_days * @booking.superpower.renting_price
+  
+      if current_user.credits >= total_price
+        ActiveRecord::Base.transaction do
+          current_user.update!(credits: current_user.credits - total_price)
+          @booking.superpower.user.update!(credits: @booking.superpower.user.credits + total_price)
+        end
+      else
+        flash[:alert] = "Not enough credits for this transaction!."
+        redirect_to bookings_path
+      end
+    end
+  end
 
   def set_booking
     @booking = Booking.find(params[:id])
